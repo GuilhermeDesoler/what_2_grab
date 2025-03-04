074b240c39f995d000da3b5d898a1eec3c284bc4
Guilherme Lima Desoler <guilherme.desoler@primum.edu.br>
2025-03-04 18:44:44 -0300

    feat: add firebase project config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:what_2_grab/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'What2Grab',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
